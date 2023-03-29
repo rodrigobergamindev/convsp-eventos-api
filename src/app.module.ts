80d92c3419a2852fca32197b39e087704b75f587
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2023-03-29 16:06:37 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/module/prisma.module';
 import { UserModule } from './user/module/user.module';
 import { EventModule } from './event/module/event.module';
-import { TicketModule } from './ticket/ticket.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 
 
 
 
 @Module({
-  imports: [PrismaModule, UserModule, EventModule, TicketModule, SubscriptionModule],
+  imports: [PrismaModule, UserModule, EventModule, SubscriptionModule],
   controllers: [],
   providers: [],
 })
